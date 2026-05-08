2266728a9488dac263a8c509592cdb8a5b76083f
pjagnus <pagnus@gmail.com>
2026-05-08 20:44:33 -0300

    tercer commit

--- db.py
+++ db.py
@@ -5,4 +5,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_conn(): 
     print(DATABASE_URL)	
-    return psycopg2.connect("postgresql://postgres:xQq7psf5A98LYYT1@db.uwnzamzijsbtkikioavg.supabase.co:5432/postgres", sslmode="require")
+    URL = "postgresql://postgres.uwnzamzijsbtkikioavg:xQq7psf5A98LYYT1@aws-1-us-west-2.pooler.supabase.com:6543/postgres"
+    #URL_OLD="postgresql://postgres:xQq7psf5A98LYYT1@db.uwnzamzijsbtkikioavg.supabase.co:5432/postgres", sslmode="require"
+    return psycopg2.connect(URL)
+    
