b6edcdf57cb121c750244e0869027978697e4f39
pjagnus <pagnus@gmail.com>
2026-05-19 20:23:35 -0300

    Actualización de la aplicación

--- app.py
+++ app.py
@@ -34,7 +34,45 @@ def nuevo():
         return redirect(url_for("index"))
 
     return render_template("form.html", usuario=None)
+    
 
+@app.route("/nuevoGrupo", methods=["GET", "POST"])
+def nuevoGrupo():
+    if request.method == "POST":
+        nro = request.form["id"]
+
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO grupos (id,jefe) VALUES (%s, %s)",
+            (nro, 0)
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return redirect(url_for("grupos"))
+
+    return render_template("formGrupo.html", usuario=None)
+
+
+@app.route("/grupos", methods=["GET", "POST"])
+def grupos():
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT id, jefe FROM grupos ORDER BY id;")
+    grupos = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template("grupos.html", grupos=grupos)
+
+@app.route("/socios", methods=["GET", "POST"])
+def socios():
+    return redirect(url_for("index"))
+
+@app.route("/consultas", methods=["GET", "POST"])
+def consultas():
+    return redirect(url_for("index"))
 
 @app.route("/editar/<int:id>", methods=["GET", "POST"])
 def editar(id):
@@ -60,6 +98,52 @@ def editar(id):
     conn.close()
 
     return render_template("form.html", usuario=usuario)
+
+@app.route("/modificarGrupo/<int:id>", methods=["GET", "POST"])
+def modificarGrupo(id):
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT id, jefe FROM grupos ORDER BY id;")
+    grupos = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template("grupos.html", grupos=grupos)
+
+@app.route("/integrantes/<int:id>", methods=["GET", "POST"])
+def integrantes(id):
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM socios where grupo =%s", (id,))
+    integrantes = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template("integrantes.html", grupo=id, integrantes=integrantes)
+    
+
+
+@app.route("/nuevoIntegrante/<int:id>", methods=["GET", "POST"])
+def nuevoIntegrante(id):
+    if request.method == "POST":
+        nombre = request.form["nombre"]
+        parentesco = request.form["parentesco"]
+        fecNac = request.form["fecNac"]
+        domicilio = request.form["domicilio"]
+        grupo = request.form["grupo"]
+
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO socios (id, nombre, parentesco, fecha_nac, domicilio, grupo) VALUES (%s, %s, %s, %s, %s, %s)",
+            (1, nombre, parentesco, fecNac, domicilio, grupo)
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return redirect(url_for("integrantes", id = grupo))
+
+    return render_template("formIntegrante.html", grupo=id)
+  
 
 
 if __name__ == "__main__":
